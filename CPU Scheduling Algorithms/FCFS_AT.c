02e68abc48070c783ff2c83e68839c0fa354bdc6
SrinikaSharma <93469490+SrinikaSharma@users.noreply.github.com>
2022-05-24 22:30:45 +0530

    FCFS with arrival time

---
+++ CPU Scheduling Algorithms/FCFS_AT.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+void swap(int* x, int* y) {
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+int main() {
+	int n, i, j,av_wt = 0, av_at = 0,  p[10], bt[10], at[10], tat[10], wt[10], st[10];
+	printf("Enter no of process : ");
+	scanf("%d",&n);
+	printf("Enter Burst time and arrival time : ");
+	for (i = 0; i < n; i++) {
+		scanf("%d %d", &bt[i], &at[i]);
+		p[i] = i;
+	}
+	for (i = 0; i < n; i++) {
+		for (j = i + 1; j < n; j++) {
+			if (at[i] > at[j]) {
+				swap(&at[i], &at[j]);
+				swap(&bt[i], &bt[j]);
+				swap(&p[i], &p[j]);
+			}
+		}
+	}
+	st[i] = bt[i];
+	st[0] = at[0];
+	wt[0] = 0;
+	
+	for (i = 1; i < n; i++) {
+		st[i] = st[i - 1] + bt[i - 1];
+		wt[i] = st[i] - at[i];
+	
+	}
+	for (i = 0; i < n; i++) {
+		tat[i] = wt[i] + bt[i];
+	}
+	printf("process\twaiting time\t total arrival time\n");
+	for (i = 0; i < n; i++) {
+		printf("%d\t%d\t%d\t\n", p[i], wt[i], tat[i]);
+		av_wt += wt[i];
+		av_at += tat[i];
+	}
+	
+	printf("Average waiting time : %d\n", av_wt/n);
+	printf("Average tat : %d\n", av_at/n);
+}
+
+/* Output
+Enter no of process : 5
+Enter Burst time and arrival time : 6
+2
+3
+5
+8
+1
+3
+0
+4
+4
+process waiting time     total arrival time
+3       0       3
+2       2       10
+0       9       15
+4       13      17
+1       16      19
+Average waiting time : 8
+Average tat : 12
+
+--------------------------------
+*/
